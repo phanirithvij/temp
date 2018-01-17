14fbaafc777f7ca177cf1112d14127558b7d75a9
Phani Rithvij <phanirithvij2000@gmail.com>
2018-01-17 18:11:14 +0530

    UPDATING THE CORE

--- procstart.sh
+++ procstart.sh
@@ -2,9 +2,10 @@ git clone https://github.com/phanirithvij/twist.moe.git
 cd twist.moe/
 git clone https://github.com/phanirithvij/temp.git
 cd temp/
+cd ~
 echo 'machine github.com' >> .netrc
 echo 'login phanirithvij' >> .netrc
 echo 'password kpr918mrrbmkrkm' >> .netrc
+cd -
 chmod +x *.sh
 ./f.sh
-
