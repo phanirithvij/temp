bfbbfcefa541d7f70af22db46c1049b3725fcd95
Phani Rithvij <phanirithvij2000@gmail.com>
2018-01-17 18:09:59 +0530

    USE THIS TO START A PROCESS

---
+++ procstart.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/phanirithvij/twist.moe.git
+cd twist.moe/
+git clone https://github.com/phanirithvij/temp.git
+cd temp/
+echo 'machine github.com' >> .netrc
+echo 'login phanirithvij' >> .netrc
+echo 'password kpr918mrrbmkrkm' >> .netrc
+chmod +x *.sh
+./f.sh
+
